7fb0aa39419613a21fe32b83a6da3d322e910ccf
pouyan <mikespro234@gmail.com>
2023-11-30 00:09:22 -0800

    structure project

---
+++ digika.web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace digika.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
